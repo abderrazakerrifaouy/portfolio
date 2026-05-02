3eddd9d3e3042410a8b25d1e863c9772ac83a8e1
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-05-02 15:55:59 +0100

    fixed configuration vit for container

--- frontEnd/app/vite.config.js
+++ frontEnd/app/vite.config.js
@@ -8,5 +8,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // Zid had l-partie hna
+  server: {
+    host: '0.0.0.0', // Darouri bach y-listen f ga3 l-interfaces
+    port: 5173,      // T-akad bli hwa l-port lli m-mappy f Docker
+    watch: {
+      usePolling: true, // Mzyana ila knti khdam b Docker volumes f Windows/macOS
+    },
   }
 })
